96c98fbb70d5ccd0c5c66cc6dfc171fee65cf4b3
Patrick Carriello <patrickcarriello@gmail.com>
2024-04-26 17:29:54 -0300

    adicionados o esquema e os códigos do sensor DHT11

---
+++ arduino/dht11_async.ino
@@ -0,0 +1,44 @@
+#include "DHT_Async.h"
+
+#define PIN_DHT11 3
+
+DHT_Async dht11a(PIN_DHT11, DHT_TYPE_11);
+float temperatura = 0;
+float humidade = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // Atualiza o sensor DHT
+  lerDHT();
+
+  // Outras execuções
+
+  delay(1);
+}
+
+// Efetua a leitura do sensor
+void lerDHT()
+{
+  static unsigned long tempo = millis();
+
+  // Mede a cada 4 segundos
+  if (millis() - tempo > 4000ul)
+  {
+    unsigned long tempoInicial = millis();
+    if (dht11a.measure(&temperatura, &humidade))
+    {
+      tempo = millis();
+      Serial.print("Temperatura: ");
+      Serial.print(temperatura);
+      Serial.print("°C, Humidade: ");
+      Serial.print(humidade);
+      Serial.print("%, Tempo leitura: ");
+      Serial.println(millis() - tempoInicial);
+    }
+  }
+}
